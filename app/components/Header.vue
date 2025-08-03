4cefc9bb5beb46ebc5ad508fbf32df4234d98292
hassan95eb <19h.amini95@gmail.com>
2025-08-03 19:23:32 +0330

    change and improve the Header component in a Vue.js application

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,21 +1,13 @@
 <template>
-  <header class="w-full h-[70px] md:h-[110px] bg-black-200">
+  <header class="w-full h-[70px] md:h-[110px] bg-black-200 p-2">
     <div class="flex h-full py-2 justify-between items-center">
-      <div class="flex-1 h-full">
-        <NuxtLink to="/" class="h-full">
-          <NuxtImg
-            src="/images/feloraphoto-logo.png"
-            class="h-full"
-            alt="felora logo"
-          />
-        </NuxtLink>
-      </div>
-      <div class="flex-6">
-        <div class="lg:w-1/2 mx-auto">
+      <div class="w-1/2 md:w-1/6"></div>
+      <div class="hidden md:block md:w-4/6">
+        <div class="hidden md:block md:w-1/2 mx-auto">
           <ul class="flex justify-between items-center">
             <li
               v-for="nav_link in navigationData.nav_links"
-              class="text-white text-xl relative whitespace-nowrap group"
+              class="text-white text-xl relative whitespace-nowrap group hover:border-b-2 hover:border-feloratext"
             >
               <NuxtLink :to="nav_link.path" :key="nav_link.path">
                 {{ nav_link.title }}
@@ -23,11 +15,11 @@
               <!--sub_menu-->
               <ul
                 v-if="nav_link.sub_links"
-                class="flex-col absolute top-full box-border -left-10 border border-feloratext rounded transition-all duration-300 ease-in-out opacity-0 invisible group-hover:visible group-hover:opacity-100"
+                class="flex-col absolute top-full box-border -left-10 rounded transition-all duration-300 ease-in-out opacity-0 invisible group-hover:visible group-hover:opacity-100"
               >
                 <li
                   v-for="sub_nav_link in nav_link.sub_links"
-                  class="text-violet-600 text-lg text-center whitespace-nowrap px-3 py-2 hover:text-feloratext hidden group-hover:block"
+                  class="text-lg text-center whitespace-nowrap px-3 py-2 hover:text-feloratext hidden group-hover:block"
                 >
                   <NuxtLink :to="sub_nav_link.path" :key="sub_nav_link.path">
                     {{ sub_nav_link.title }}
@@ -39,7 +31,15 @@
           </ul>
         </div>
       </div>
-      <div class="flex-1"></div>
+      <div class="w-1/2 md:w-1/6 h-full">
+        <NuxtLink to="/" class="h-full">
+          <NuxtImg
+            src="/images/feloraphoto-logo.png"
+            class="h-full mx-auto ml-2"
+            alt="felora logo"
+          />
+        </NuxtLink>
+      </div>
     </div>
   </header>
 </template>
